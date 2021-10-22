725c931582d4e4179ceb518ddeeeb7221c8d6e73
MasterJD117 <UKF117@gmail.com>
2021-10-21 21:15:35 -0600

    primer commit

---
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { GifsService } from '../../gifs/services/gifs.service';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+})
+export class SidebarComponent {
+
+  constructor(private gifsServices:GifsService) { }
+  
+  get historial(){
+    return this.gifsServices.historial;
+  }
+  buscar(termino: string){
+
+    this.gifsServices.buscarGifs(termino);
+  }
+}
